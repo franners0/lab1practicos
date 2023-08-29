5f25279c94705117e0b855e7bc11ee12c5eb25a7
franners <franciscopiracesvega@gmail.com>
2023-08-29 09:58:20 -0300

    tp3 y tp4

---
+++ target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"practico3","c":"Iniciarsesion","l":"Iniciarsesion()","url":"%3Cinit%3E()"},{"p":"practico3","c":"Iniciarsesion","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"practico3","l":"Iniciarsesion"}]
